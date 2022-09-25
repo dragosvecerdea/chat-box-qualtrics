6dae47dab175677cffbc4f0a2c038c165e78019c
Dragos Vecerdea <dragos@Dragoss-Mac-mini.local>
2022-09-25 21:14:44 +0200

    extend app api

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+const app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+app.get('api/history/:roomId', (req, res) => {
+  res.send(req.params)
+})
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
